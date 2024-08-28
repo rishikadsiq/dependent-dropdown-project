4b3f99f4b77899e61eec1ec74873b6fa341d0650
rishikadsiq <rishika@mediaamp.co.in>
2024-08-28 15:46:53 +0530

    Api integrate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Demo from './components/Demo'
+import Container from 'react-bootstrap/Container';
 function App() {
   
 
   return (
-    <>
+    <Container>
       <Demo />
-    </>
+    </Container>
   )
 }
 
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,102 +1,142 @@
 import * as React from 'react';
 import { Grid, GridColumn as Column, GridToolbar } from '@progress/kendo-react-grid';
+import { Button } from '@progress/kendo-react-buttons';
 // @ts-expect-error
-import { MyCommandCell } from './shared-gd-myCommandCell';
-// @ts-expect-error
-import { insertItem, getItems, updateItem, deleteItem } from './shared-gd-services';
-import { Button } from "@progress/kendo-react-buttons";
-const editField = 'inEdit';
+import { sampleProducts } from './shared-gd-sample-products';
 // @ts-expect-error
 
-const CommandCell = props => {
-  const {
-    edit,
-    remove,
-    add,
-    discard,
-    update,
-    cancel,
-    editField
-  } = props;
-  return <MyCommandCell {...props} edit={edit} remove={remove} add={add} discard={discard} update={update} cancel={cancel} editField={editField} />;
+const CustomCell = ({
+  tdProps,
+  children,
+  color
+}) => {
+  return tdProps ? <td {...tdProps} style={{
+    ...tdProps.style,
+    backgroundColor: color
+  }}>
+      {children}
+    </td> : null;
 };
+const MyInputCustomCell = props => <CustomCell {...props} color="red" />;
+const MyNumericCustomCell = props => <CustomCell {...props} color="lightgreen" />;
+const MyBooleanCustomCell = props => <CustomCell {...props} color="pink" />;
+const MyDateCustomCell = props => <CustomCell {...props} color="lightblue" />;
 const Demo = () => {
-  const [data, setData] = React.useState([]);
+  const [data, setData] = React.useState();
   React.useEffect(() => {
-    let newItems = getItems();
-    setData(newItems);
+    const fetchData = async () => {
+      const response = await fetch("192.168.1.4:5000/metalist", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData();
   }, []);
-
-  // modify the data in the store, db etc
-  const remove = dataItem => {
-    const newData = [...deleteItem(dataItem)];
-    setData(newData);
-  };
-  const add = dataItem => {
-    dataItem.inEdit = true;
-    const newData = insertItem(dataItem);
-    setData(newData);
-  };
-  const update = dataItem => {
-    dataItem.inEdit = false;
-    const newData = updateItem(dataItem);
-    setData(newData);
-  };
-
-  // Local state operations
-  const discard = () => {
-    const newData = [...data];
-    newData.splice(0, 1);
-    setData(newData);
-  };
-  const cancel = dataItem => {
-    const originalItem = getItems().find(p => p.ProductID === dataItem.ProductID);
-    const newData = data.map(item => item.ProductID === originalItem.ProductID ? originalItem : item);
-    setData(newData);
+  const enterInsert = () => {
+    const maxId = data.reduce((acc, current) => Math.max(acc, current.ID || 0), 0);
+    const dataItem = {
+      ID: maxId + 1,
+      inEdit: true
+    };
+    const newProducts = [dataItem, ...data];
+    setData(newProducts);
   };
   const enterEdit = dataItem => {
-    setData(data.map(item => item.ProductID === dataItem.ProductID ? {
+    const newData = data.map(item => item.ID === dataItem.ID ? {
       ...item,
       inEdit: true
-    } : item));
-  };
-  const itemChange = event => {
-    const newData = data.map(item => item.ProductID === event.dataItem.ProductID ? {
-      ...item,
-      [event.field || '']: event.value
     } : item);
     setData(newData);
   };
-  const addNew = () => {
-    const newDataItem = {
-      inEdit: true,
-      Discontinued: false
-    };
-    setData([newDataItem, ...data]);
+  const save = dataItem => {
+    const newData = data.map(item => item.ID === dataItem.ID ? {
+      ...dataItem,
+      inEdit: undefined
+    } : item);
+    setData(newData);
   };
-  const commandCellProps = {
-    edit: enterEdit,
-    remove: remove,
-    add: add,
-    discard: discard,
-    update: update,
-    cancel: cancel,
-    editField: editField
+  const cancel = dataItem => {
+    if (!dataItem.ID) {
+      setData(data.filter(item => item.ID !== undefined));
+    } else {
+      const newData = data.map(item => item.ID === dataItem.ID ? {
+        ...data.find(p => p.ID === dataItem.ID),
+        inEdit: false
+      } : item);
+      console.log(newData);
+      setData(newData);
+    }
   };
-  return <Grid style={{
-    height: '420px'
-  }} data={data} onItemChange={itemChange} editField={editField}>
-      <GridToolbar>
-        <Button title="Add new" themeColor={"primary"} onClick={addNew} type="button">
-          Add new
-        </Button>
-      </GridToolbar>
-      <Column field="ProductID" title="Id" width="50px" editable={false} />
-      <Column field="ProductName" title="Product Name" width="200px" />
-      <Column field="FirstOrderedOn" title="First Ordered" editor="date" format="{0:d}" width="150px" />
-      <Column field="UnitsInStock" title="Units" width="120px" editor="numeric" />
-      <Column field="Discontinued" title="Discontinued" editor="boolean" />
-      <Column cell={props => <CommandCell {...props} {...commandCellProps} />} width="200px" />
-    </Grid>;
+  const remove = dataItem => {
+    setData(data.filter(item => item.ID !== dataItem.ID));
+  };
+  const itemChange = event => {
+    const {
+      value,
+      field
+    } = event;
+    if (!field) return;
+    const newData = data.map(item => item.ID === event.dataItem.ID ? {
+      ...item,
+      [field]: value
+    } : item);
+    setData(newData);
+  };
+  React.useEffect(() => {
+    enterInsert();
+  }, []);
+  const MyCommandCell = props => <td>
+      {!props.dataItem.inEdit ? <>
+          <Button themeColor={'primary'} onClick={() => enterEdit(props.dataItem)}>
+            Edit
+          </Button>
+          <Button onClick={() => window.confirm('Confirm deleting ' + props.dataItem.TaskName) && remove(props.dataItem)}>
+            Remove
+          </Button>
+        </> : <>
+          <Button themeColor={'primary'} onClick={() => save(props.dataItem)}>
+            {props.dataItem.ID ? 'Update' : 'Add'}
+          </Button>
+          <Button onClick={() => cancel(props.dataItem)}>
+            {props.dataItem.ID ? 'Cancel' : 'Discard changes'}
+          </Button>
+        </>}
+    </td>;
+  return <div>
+      <Grid style={{
+      height: '400px'
+    }} data={data} onItemChange={itemChange} editField="inEdit" cells={{
+      edit: {
+        text: MyInputCustomCell,
+        numeric: MyNumericCustomCell,
+        boolean: MyBooleanCustomCell,
+        date: MyDateCustomCell
+      }
+    }}>
+        <GridToolbar>
+          <Button title="Add new" type="button" onClick={enterInsert}>
+            Add new
+          </Button>
+        </GridToolbar>
+        <Column field="ID" title="Id" editable={false} />
+        <Column field="ClientName" title="Client Name" editor="text" />
+        <Column field="ProjectName" title="Project Name" editor="text" />
+        <Column field="TaskName" title="Task Name" editor="text" />
+        <Column field="Mon" title="Mon" editor="numeric" />
+        <Column field="Tue" title="Tue" editor="numeric" />
+        <Column field="Wed" title="Wed" editor="numeric" />
+        <Column field="Thu" title="Thu" editor="numeric" />
+        <Column field="Fri" title="Fri" editor="numeric" />
+        <Column field="Sat" title="Sat" editor="numeric" />
+        <Column field="Sun" title="Sun" editor="numeric" />
+        <Column field="StartDate" title="Start Date" editor="date" format="{0:d}" />
+        <Column cell={MyCommandCell} width="200px" />
+      </Grid>
+    </div>;
 };
 export default Demo;
+
