2236b96adf571e0d5a4629ef754ea3683eb61604
rishikadsiq <rishika@mediaamp.co.in>
2024-10-04 12:15:37 +0530

    Stats api call in Home page added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,15 +9,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUsers, faTasks, faProjectDiagram, faUserTie, faClipboardCheck, faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
 import HeaderLayout from './HeaderLayout';
 import Alerts from '../dynamic-compoenents/Alerts';
+import { GetRequestHelper } from '../helper/GetRequestHelper';
 
 const Home = () => {
   const [showAlert, setShowAlert] = React.useState(false);
   const [message, setMessage] = React.useState("");
   const [variant, setVariant] = React.useState(null);
   const navigate = useNavigate();
-  const [data, setData] = React.useState({
+  const [data, setData] = React.useState({})
+  const [userType, setUserType] = React.useState()
+  const [cardData, setCardData] = React.useState([])
 
-  })
+
 
   useEffect(() => {
     const login_message = JSON.parse(localStorage.getItem('login_message'));
@@ -27,16 +30,55 @@ const Home = () => {
       setVariant('success');
       localStorage.removeItem('login_message');
     }
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    if(userData){
+      if(userData.role === 'Admin'){
+        setUserType(userData.role)
+      }else(
+        setUserType('normal_user')
+      )
+    }
+
   }, []);
 
-  const cardsData = [
-    { title: 'Clients', total: 'Total Clients', active: 'Active Clients', icon: faUsers, navigate: 'clients' },
-    { title: 'Projects', total: 'Total Projects', active: 'Active Projects', icon: faProjectDiagram, navigate: 'projects' },
-    { title: 'Tasks', total: 'Total Tasks', active: 'Active Tasks', icon: faTasks, navigate: 'tasks' },
-    { title: 'Employees', total: 'Total Employees', active: 'Active Employees', icon: faUserTie, navigate: 'users' },
-    { title: 'Timesheets', total: 'Total Timesheets', active: 'Pending Timesheets', icon: faCalendarCheck, navigate: 'timesheets' },
-    { title: 'Approvals', total: 'Total Approvals', active: 'Pending Approvals', icon: faClipboardCheck, navigate: 'approvals' }
-  ];
+  const getListing = async() => {
+    try {
+        const data1 = await GetRequestHelper('getstats', navigate);
+        if (data1.status === 404) {
+            setData([]);
+        } else {
+          console.log(data1)
+            setData(data1.stats_data);
+          }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            setData([]); 
+        }
+    };
+
+  React.useEffect(() => {
+    getListing(); // Call the function to fetch data
+}, []);
+
+useEffect(() => {
+  if(userType==='Admin'){
+    setCardData([
+      { title: 'Clients', total: 'Total Clients', active: 'Active Clients', icon: faUsers, navigate: 'clients', total_data: 'total_clients', active_data: 'active_clients' },
+      { title: 'Projects', total: 'Total Projects', active: 'Active Projects', icon: faProjectDiagram, navigate: 'projects', total_data: 'total_projects', active_data: 'active_projects' },
+      { title: 'Tasks', total: 'Total Tasks', active: 'Active Tasks', icon: faTasks, navigate: 'tasks', total_data: 'total_tasks', active_data: 'active_tasks' },
+      { title: 'Employees', total: 'Total Employees', active: 'Active Employees', icon: faUserTie, navigate: 'users', total_data: 'total_employees', active_data: 'active_employees' },
+      { title: 'Timesheets', total: 'Total Timesheets', active: 'Pending Timesheets', icon: faCalendarCheck, navigate: 'timesheets', total_data: 'total_timesheet', active_data: 'total_pending_approvals' },
+      { title: 'Approvals', total: 'Total Approvals', active: 'Pending Approvals', icon: faClipboardCheck, navigate: 'approvals', total_data: 'total_approver_timesheets', active_data: 'total_approver_pending_approvals' }
+    ])
+  } else if(userType==='normal_user'){
+    setCardData([
+     { title: 'Timesheets', total: 'Total Timesheets', active: 'Pending Timesheets', icon: faCalendarCheck, navigate: 'timesheets', total_data: 'total_timesheet', active_data: 'total_pending_approvals' },
+      { title: 'Approvals', total: 'Total Approvals', active: 'Pending Approvals', icon: faClipboardCheck, navigate: 'approvals', total_data: 'total_approver_timesheets', active_data: 'total_approver_pending_approvals' }
+    ])
+  }
+},[userType])
+
+
 
   const handleNavigate = (link) => {
     navigate(`/${link}`);
@@ -52,7 +94,7 @@ const Home = () => {
       </div>
 
       <Row className="g-4 text-center">
-        {cardsData.map((card, index) => (
+        {cardData.map((card, index) => (
           <Col key={index} sm={12} md={6} lg={4}>
             <Card className="h-100 text-center shadow" onClick={() => handleNavigate(card.navigate)}>
               <Card.Body>
@@ -61,8 +103,8 @@ const Home = () => {
               </Card.Body>
               <Card.Footer className="bg-transparent">
                 <ListGroup className="list-group-flush">
-                  <ListGroup.Item>{card.total}</ListGroup.Item>
-                  <ListGroup.Item>{card.active}</ListGroup.Item>
+                  <ListGroup.Item>{card.total}: {data[card.total_data]}</ListGroup.Item>
+                  <ListGroup.Item>{card.active}: {data[card.active_data]}</ListGroup.Item>
                   <ListGroup.Item>
                     <Button variant="link" className="text-primary">More Details...</Button>
                   </ListGroup.Item>
